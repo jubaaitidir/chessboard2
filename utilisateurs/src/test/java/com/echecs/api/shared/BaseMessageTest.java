e5dbb52d9c4f44b1ce9531509e6ac98f85d0c380
Juba ait idir <jubaaitidir@gmail.com>
2021-10-29 10:45:08 +0200

    test local

--- utilisateurs/src/test/java/com/echecs/api/shared/BaseMessageTest.java
+++ utilisateurs/src/test/java/com/echecs/api/shared/BaseMessageTest.java
@@ -1,7 +1,5 @@
 package com.echecs.api.shared;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BaseMessageTest {
 
 }
--- utilisateurs/src/test/java/com/echecs/api/shared/BaseUserTest.java
+++ utilisateurs/src/test/java/com/echecs/api/shared/BaseUserTest.java
@@ -1,6 +1,5 @@
 package com.echecs.api.shared;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class BaseUserTest {
 
